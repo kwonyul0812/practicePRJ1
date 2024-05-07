c4ff83ec3342dd1f41eefbad04478f9b72a44d2a
kwonyul <kw990812@gmail.com>
2024-05-07 11:35:04 +0900

    first commit

---
+++ src/main/java/com/practiceprj1/PracticePrj1Application.java
@@ -0,0 +1,13 @@
+package com.practiceprj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticePrj1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticePrj1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/practiceprj1/PracticePrj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practiceprj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticePrj1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
